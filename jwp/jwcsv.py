f627f5b04783e67f6033e5c44c75a5cac9699c11
jameswenzel <wenzel.james.r@gmail.com>
2016-06-17 21:38:19 -0400

    Added Tor to soup modules, updated gitignore, added headers to csv?

--- jwp/jwcsv.py
+++ jwp/jwcsv.py
@@ -3,39 +3,41 @@ import csv
 
 class jwcsv(object):
 
-    def write_csv(self, outfile, rows, dict_headers=[], headers=[]):
+    def write_csv(self, outfile, rows, dict_headers=[], headers=[], encoding='utf-8'):
         '''Basis for writing out csv in a subclass'''
         if '.csv' in outfile:
             outfile = outfile.split('.csv')[0]
         if dict_headers:
-            with open('%s.csv' % outfile, 'w', encoding='utf-8') as outfile:
+            with open('%s.csv' % outfile, 'w', encoding=encoding) as outfile:
                 writer = csv.DictWriter(outfile, dict_headers)
+                writer.writeheader()
                 writer.writerows(rows)
         else:
-            with open('%s.csv' % outfile, 'w', encoding='utf-8') as outfile:
+            with open('%s.csv' % outfile, 'w', encoding=encoding) as outfile:
                 writer = csv.writer(outfile)
                 writer.writerows(headers) if headers else None
                 writer.writerows(rows)
 
-    def read_csv(self, infile):
-        reader = csv.reader(open(infile, encoding='utf-8'), delimiter=',')
+    def read_csv(self, infile, encoding='utf-8'):
+        reader = csv.reader(open(infile, encoding=encoding), delimiter=',')
         return list(list(row for row in reader))
 
 
-def write_csv(outfile, rows, dict_headers=[], headers=[]):
+def write_csv(outfile, rows, dict_headers=[], headers=[], encoding='utf-8'):
     if '.csv' in outfile:
         outfile = outfile.split('.csv')[0]
     if dict_headers:
-        with open('%s.csv' % outfile, 'w', encoding='utf-8') as outfile:
+        with open('%s.csv' % outfile, 'w', encoding=encoding) as outfile:
             writer = csv.DictWriter(outfile, dict_headers)
+            writer.writeheader()
             writer.writerows(rows)
     else:
-        with open('%s.csv' % outfile, 'w', encoding='utf-8') as outfile:
+        with open('%s.csv' % outfile, 'w', encoding=encoding) as outfile:
             writer = csv.writer(outfile)
             writer.writerow(headers) if headers else None
             writer.writerows(rows)
 
 
-def read_csv(infile):
-    reader = csv.reader(open(infile, encoding='utf-8'), delimiter=',')
+def read_csv(infile, encoding='utf-8'):
+    reader = csv.reader(open(infile, encoding=encoding), delimiter=',')
     return list(list(row for row in reader))
--- jwp/jwsoup.py
+++ jwp/jwsoup.py
@@ -1,18 +1,34 @@
 import requests
 # import mechanicalsoup
 from bs4 import BeautifulSoup
+import socks
+import socket
 
+# Configuration
+SOCKS5_PROXY_HOST = '127.0.0.1'
+SOCKS5_PROXY_PORT = 9050
+socks.set_default_proxy(socks.SOCKS5, SOCKS5_PROXY_HOST, SOCKS5_PROXY_PORT)
+
+DEFAULT_SOCKET = socket.socket
+TOR_SOCKET = socks.socksocket
+
+s = requests.Session()  # use a session for persistence
 
 cookies = None
-user_agent = 'SoupStock'
+user_agent = 'test'
 
 
 class jwsoup(object):
     cookies = None
     user_agent = 'SoupStock'
+    s = requests.Session()
 
-    def get_soup(self, url, fail=False):
-        req = requests.get(
+    def get_soup(self, url, fail=False, tor=False):
+        if tor and socket.socket is not TOR_SOCKET:
+            use_tor()
+        elif socket.socket is not DEFAULT_SOCKET:
+            reset_tor()
+        req = self.s.get(
             url, headers={'User-Agent': self.user_agent},
             cookies=self.cookies)
         if req.status_code == 200 or req.status_code == 410:
@@ -23,10 +39,13 @@ class jwsoup(object):
                 raise ValueError('Status ' + str(req.status_code))
             return BeautifulSoup('', 'lxml')
 
-    @staticmethod
-    def get_cookies(url):
+    def get_cookies(self, url, tor=False):
+        if tor and socket.socket is not TOR_SOCKET:
+            use_tor()
+        elif socket.socket is not DEFAULT_SOCKET:
+            reset_tor()
         "gets cookie for passing with request otherwise site might block acces"
-        req = requests.get(
+        req = self.s.get(
             url, headers={'User-Agent': 'jwsoup'})
         if req.status_code == 200:
             return req.cookies
@@ -50,8 +69,12 @@ Experimental login method using mechanicalsoup for a project I was working on.
 '''
 
 
-def get_soup(url, cookies=None, fail=False):
-    req = requests.get(
+def get_soup(url, cookies=None, fail=False, tor=False):
+    if tor and socket.socket is not TOR_SOCKET:
+        use_tor()
+    elif socket.socket is not DEFAULT_SOCKET:
+        reset_tor()
+    req = s.get(
         url, headers={'User-Agent': user_agent},
         cookies=cookies)
     if req.status_code == 200 or req.status_code == 410:
@@ -63,12 +86,31 @@ def get_soup(url, cookies=None, fail=False):
         return BeautifulSoup('', 'lxml')
 
 
-def get_cookies(url):
-    "gets cookie for passing with request otherwise site might block acces"
-    req = requests.get(
+def get_cookies(url, tor=False):
+    "gets cookie for passing with request otherwise site might block access"
+    if tor and socket.socket is not TOR_SOCKET:
+        use_tor()
+    elif socket.socket is not DEFAULT_SOCKET:
+        reset_tor()
+    req = s.get(
         url, headers={'User-Agent': 'jwsoup'})
     if req.status_code == 200:
         return req.cookies
     else:
         print('Unable to download url ' + url)
         return None
+
+
+def use_tor():
+    "use tor proxy port"
+    # Set up a proxy
+    socket.socket = TOR_SOCKET
+
+
+def reset_tor():
+    "return to default socket"
+    socket.socket = DEFAULT_SOCKET
+
+
+def get_new_IP():
+    pass
