af8900d1212120709f77f0c255f2545622177351
jameswenzel <wenzel.james.r@gmail.com>
2016-06-21 14:04:50 -0400

    added delimeter params

--- jwp/jwcsv.py
+++ jwp/jwcsv.py
@@ -3,7 +3,7 @@ import csv
 
 class jwcsv(object):
 
-    def write_csv(self, outfile, rows, dict_headers=[], headers=[], encoding='utf-8'):
+    def write_csv(self, outfile, rows, dict_headers=[], headers=[], delimiter=',', encoding='utf-8'):
         '''Basis for writing out csv in a subclass'''
         if '.csv' in outfile:
             outfile = outfile.split('.csv')[0]
@@ -18,26 +18,26 @@ class jwcsv(object):
                 writer.writerows(headers) if headers else None
                 writer.writerows(rows)
 
-    def read_csv(self, infile, encoding='utf-8'):
-        reader = csv.reader(open(infile, encoding=encoding), delimiter=',')
+    def read_csv(self, infile, encoding='utf-8', delimiter=','):
+        reader = csv.reader(open(infile, encoding=encoding), delimiter=delimiter)
         return list(list(row for row in reader))
 
 
-def write_csv(outfile, rows, dict_headers=[], headers=[], encoding='utf-8'):
+def write_csv(outfile, rows, dict_headers=[], headers=[], delimiter=',', encoding='utf-8'):
     if '.csv' in outfile:
         outfile = outfile.split('.csv')[0]
     if dict_headers:
         with open('%s.csv' % outfile, 'w', encoding=encoding) as outfile:
-            writer = csv.DictWriter(outfile, dict_headers)
+            writer = csv.DictWriter(outfile, fieldnames=dict_headers, delimiter=delimiter)
             writer.writeheader()
             writer.writerows(rows)
     else:
         with open('%s.csv' % outfile, 'w', encoding=encoding) as outfile:
-            writer = csv.writer(outfile)
+            writer = csv.writer(outfile, delimiter=delimiter)
             writer.writerow(headers) if headers else None
             writer.writerows(rows)
 
 
-def read_csv(infile, encoding='utf-8'):
-    reader = csv.reader(open(infile, encoding=encoding), delimiter=',')
+def read_csv(infile, encoding='utf-8', delimiter=','):
+    reader = csv.reader(open(infile, encoding=encoding), delimiter=delimiter)
     return list(list(row for row in reader))
