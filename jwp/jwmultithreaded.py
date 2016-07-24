98efeddaaf67707ec5b6ff7f5140843f83832aad
jameswenzel <wenzel.james.r@gmail.com>
2016-07-23 20:23:56 -0400

    added maxtasksperchild and chunksize to multithread methods for better memory mgmt and more user-control

--- jwp/jwmultithreaded.py
+++ jwp/jwmultithreaded.py
@@ -14,31 +14,34 @@ class jwmultithreaded(object):
         self.cache_updated = False
 
     def multithread(self, fn, args=[[]], pool_type=Pool,
-                    processes=cpu_count()):
+                    processes=cpu_count(), maxtasksperchild=1,
+                    chunksize=1):
         '''Multithread method using a Pool. Not inherently threadsafe.
         For threadsafe operations, use Managers.
         Args must be wrapped in their own list, as starmap is used for
         multiple arguments.
         Returns a list of the results'''
 
-        with pool_type(processes) as pool:
-            results = pool.starmap(fn, args)
+        with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
+            results = pool.starmap(fn, args, chunksize=chunksize)
         # close the pool and wait for results to return
         return results
 
-    def safe_multithread(self, fn, args=[[]], processes=8):
+    def safe_multithread(self, fn, args=[[]], processes=cpu_count(), maxtasksperchild=1,
+                         chunksize=1):
         '''Guaranteed threadsafe version of multithread using ThreadPool.
         Allows child threads. Limited by interpreter lock.'''
 
         return self.multithread(fn, args=args, pool_type=ThreadPool,
-                                processes=processes)
+                                processes=processes, maxtasksperchild=maxtasksperchild,
+                                chunksize=chunksize)
 
     def multithread_failsafe(self, fn, args=[[]], pool_type=Pool,
-                             processes=cpu_count()):
+                             processes=cpu_count(), maxtasksperchild=1):
         '''Aynchronous multithreading that does not break on individual errors.
         Instead, prints error and message, and the input is disregarded'''
 
-        with pool_type(processes) as pool:
+        with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
             threads = []
             for elem in args:
                 threads.append(pool.apply_async(fn, elem))
@@ -53,12 +56,13 @@ class jwmultithreaded(object):
                     print()
         return results
 
-    def safe_multithread_failsafe(self, fn, args=[[]], processes=8):
+    def safe_multithread_failsafe(self, fn, args=[[]], processes=cpu_count(),
+                                  maxtasksperchild=1):
         '''Threadsafe version of multithread_failsafe.
         Allows child threads. Limited by interpreter lock.'''
 
         return self.multithread_failsafe(fn, args=args, pool_type=ThreadPool,
-                                         processes=processes)
+                                         processes=processes, maxtasksperchild=maxtasksperchild)
 
 
 def multithread(fn, args=[[]], pool_type=Pool,
@@ -106,10 +110,10 @@ def safe_multithread_failsafe(fn, args=[[]], processes=4):
     return multithread_failsafe(fn, args=args, pool_type=ThreadPool,
                                 processes=processes)
 
-
 # http://stackoverflow.com/questions/6974695/python-process-pool-non-daemonic
 # Experimental for processes that spawn children. In my experience,
 # often results in broken pipes.
+
 
 class NoDaemonProcess(multiprocessing.Process):
     # make 'daemon' attribute always return False
