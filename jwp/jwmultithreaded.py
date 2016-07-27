4cabab92284bbf2bbfe8cec23b6732da38aede3d
jameswenzel <wenzel.james.r@gmail.com>
2016-07-27 17:11:16 -0400

    fixed maxtasksperchildren bug with safe_multithread functions/methods, consistently set default processes to cpu_count

--- jwp/jwmultithreaded.py
+++ jwp/jwmultithreaded.py
@@ -5,6 +5,8 @@ import traceback
 # for reference:
 # from multiprocessing import Manager, Lock
 
+cpus = cpu_count()
+
 
 class jwmultithreaded(object):
     '''Inspired by http://chriskiehl.com/article/parallelism-in-one-line/
@@ -14,7 +16,7 @@ class jwmultithreaded(object):
         self.cache_updated = False
 
     def multithread(self, fn, args=[[]], pool_type=Pool,
-                    processes=cpu_count(), maxtasksperchild=1,
+                    processes=cpus, maxtasksperchild=1,
                     chunksize=1):
         '''Multithread method using a Pool. Not inherently threadsafe.
         For threadsafe operations, use Managers.
@@ -22,31 +24,41 @@ class jwmultithreaded(object):
         multiple arguments.
         Returns a list of the results'''
 
-        with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
-            results = pool.starmap(fn, args, chunksize=chunksize)
-        # close the pool and wait for results to return
+        def helper(pool):
+            return pool.starmap(fn, args, chunksize=chunksize)
+
+        # ThreadPools do not take a maxtasksperchild argument,
+        # so we need to conditionally construct a pool
+
+        if type(pool_type) is Pool:
+            with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
+                results = helper(pool)
+        else:
+            with pool_type(processes) as pool:
+                results = helper(pool)
         return results
 
-    def safe_multithread(self, fn, args=[[]], processes=cpu_count(), maxtasksperchild=1,
+    def safe_multithread(self, fn, args=[[]], processes=cpus,
                          chunksize=1):
         '''Guaranteed threadsafe version of multithread using ThreadPool.
         Allows child threads. Limited by interpreter lock.'''
 
         return self.multithread(fn, args=args, pool_type=ThreadPool,
-                                processes=processes, maxtasksperchild=maxtasksperchild,
+                                processes=processes,
                                 chunksize=chunksize)
 
     def multithread_failsafe(self, fn, args=[[]], pool_type=Pool,
-                             processes=cpu_count(), maxtasksperchild=1):
+                             processes=cpus, maxtasksperchild=1,
+                             chunksize=1):
         '''Aynchronous multithreading that does not break on individual errors.
         Instead, prints error and message, and the input is disregarded'''
 
-        with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
-            threads = []
-            for elem in args:
-                threads.append(pool.apply_async(fn, elem))
+        def helper(pool):
+            # threads = []
+            # for elem in args:
+            #     threads.append(pool.apply_async(fn, elem))
             results = []
-            for r in threads:
+            for r in pool.starmap_async(fn, args, chunksize=chunksize):
                 try:
                     results.append(r.get())
                 except:
@@ -54,46 +66,71 @@ class jwmultithreaded(object):
                     traceback.print_exc()
                     print('######END TRACEBACK######')
                     print()
+            return results
+
+        # ThreadPools do not take a maxtasksperchild argument,
+        # so we need to conditionally construct a pool
+
+        if type(pool_type) is Pool:
+            with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
+                results = helper(pool)
+        else:
+            with pool_type(processes) as pool:
+                results = helper(pool)
+
         return results
 
-    def safe_multithread_failsafe(self, fn, args=[[]], processes=cpu_count(),
-                                  maxtasksperchild=1):
+    def safe_multithread_failsafe(self, fn, args=[[]], processes=cpus):
         '''Threadsafe version of multithread_failsafe.
         Allows child threads. Limited by interpreter lock.'''
 
         return self.multithread_failsafe(fn, args=args, pool_type=ThreadPool,
-                                         processes=processes, maxtasksperchild=maxtasksperchild)
+                                         processes=processes)
 
 
 def multithread(fn, args=[[]], pool_type=Pool,
-                processes=8):
+                processes=cpus, maxtasksperchild=1,
+                chunksize=1):
     '''Multithread method using a Pool. Not inherently threadsafe.
     For threadsafe operations, use Managers or Locks.
     Args must be wrapped in their own list, as starmap is used for
     multiple arguments.
     Returns a list of the results'''
-    with pool_type(processes) as pool:
-        results = pool.starmap(fn, args)
-    # close the pool and wait for results to return
+
+    def helper(pool):
+        return pool.starmap(fn, args, chunksize=chunksize)
+
+    # ThreadPools do not take a maxtasksperchild argument,
+    # so we need to conditionally construct a pool
+
+    if type(pool_type) is Pool:
+        with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
+            results = helper(pool)
+    else:
+        with pool_type(processes) as pool:
+            results = helper(pool)
     return results
 
 
-def safe_multithread(fn, args=[[]], processes=8):
+def safe_multithread(fn, args=[[]], processes=cpus, chunksize=1):
     '''Guaranteed threadsafe version of multithread using ThreadPool.
     Allows child threads. Limited by interpreter lock.'''
-    return multithread(fn, args=args, pool_type=ThreadPool, processes=processes)
+    return multithread(fn, args=args, pool_type=ThreadPool, processes=processes,
+                       chunksize=chunksize)
 
 
 def multithread_failsafe(fn, args=[[]], pool_type=Pool,
-                         processes=4):
+                         processes=cpus, maxtasksperchild=1,
+                         chunksize=1):
     '''Aynchronous multithreading that does not break on individual errors.
-    Instead, prints traceback, and the input is disregarded'''
-    with pool_type(processes) as pool:
-        threads = []
-        for elem in args:
-            threads.append(pool.apply_async(fn, elem))
+    Instead, prints error and message, and the input is disregarded'''
+
+    def helper(pool):
+        # threads = []
+        # for elem in args:
+        #     threads.append(pool.apply_async(fn, elem))
         results = []
-        for r in threads:
+        for r in pool.starmap_async(fn, args, chunksize=chunksize):
             try:
                 results.append(r.get())
             except:
@@ -101,12 +138,25 @@ def multithread_failsafe(fn, args=[[]], pool_type=Pool,
                 traceback.print_exc()
                 print('######END TRACEBACK######')
                 print()
+        return results
+
+    # ThreadPools do not take a maxtasksperchild argument,
+    # so we need to conditionally construct a pool
+
+    if type(pool_type) is Pool:
+        with pool_type(processes, maxtasksperchild=maxtasksperchild) as pool:
+            results = helper(pool)
+    else:
+        with pool_type(processes) as pool:
+            results = helper(pool)
+
     return results
 
 
-def safe_multithread_failsafe(fn, args=[[]], processes=4):
+def safe_multithread_failsafe(fn, args=[[]], processes=cpus, chunksize=1):
     '''Threadsafe version of multithread_failsafe.
     Allows child threads. Limited by interpreter lock.'''
+
     return multithread_failsafe(fn, args=args, pool_type=ThreadPool,
                                 processes=processes)
 
