8fb2553eb65e391bc16666c24e16b0dd40955396
jameswenzel <wenzel.james.r@gmail.com>
2016-07-29 15:54:17 -0400

    change name var to path, assert that it is type str. plus fix exception print with destroy_on_fail

--- jwp/jwcache.py
+++ jwp/jwcache.py
@@ -6,57 +6,61 @@ class jwcache(object):
     cache_updated = False
     cache = {}
 
-    def load_cache(self, name, destroy_on_fail=False):
+    def load_cache(self, path, destroy_on_fail=False):
+        assert type(path) is str, 'path must be string'
         cache_temp = {}
         try:
-            with open(name) as data_file:
+            with open(path) as data_file:
                 cache_temp = json.load(data_file)
                 print('Cache loaded successfully.')
                 return cache_temp
         except FileNotFoundError:
             print("Creating cache.")
-            with open(name, 'w', encoding='utf-16') as outfile:
+            with open(path, 'w', encoding='utf-16') as outfile:
                 json.dump(cache_temp, outfile)
             return cache_temp
         except Exception as e:
             print(type(e), e)
             if destroy_on_fail:
                 print("Loading cache failed. Overwriting corrupt cache.")
-                with open(name, 'w', encoding='utf-16') as outfile:
+                with open(path, 'w', encoding='utf-16') as outfile:
                     json.dump(cache_temp, outfile)
                 return cache_temp
             else:
                 raise e
 
-    def write_cache(self, name, cache):
+    def write_cache(self, path, cache):
+        assert type(path) is str, 'path must be string'
         if self.cache_updated:
-            with open(name, 'w', encoding='utf-16') as outfile:
+            with open(path, 'w', encoding='utf-16') as outfile:
                 json.dump(cache, outfile)
 
 
-def load_cache(name, destroy_on_fail=False):
+def load_cache(path, destroy_on_fail=False):
+    assert type(path) is str, 'path must be string'
     cache_temp = {}
     try:
-        with open(name) as data_file:
+        with open(path) as data_file:
             cache_temp = json.load(data_file)
             print('Cache loaded successfully.')
             return cache_temp
     except FileNotFoundError:
         print("Creating cache.")
-        with open(name, 'w', encoding='utf-16') as outfile:
+        with open(path, 'w', encoding='utf-16') as outfile:
             json.dump(cache_temp, outfile)
         return cache_temp
     except Exception as e:
-        print(type(e), e)
         if destroy_on_fail:
+            print(type(e), e)
             print("Loading cache failed. Overwriting corrupt cache.")
-            with open(name, 'w', encoding='utf-16') as outfile:
+            with open(path, 'w', encoding='utf-16') as outfile:
                 json.dump(cache_temp, outfile)
             return cache_temp
         else:
             raise e
 
 
-def write_cache(name, cache):
-    with open(name, 'w') as outfile:
+def write_cache(path, cache):
+    assert type(path) is str, 'path must be string'
+    with open(path, 'w') as outfile:
         json.dump(cache, outfile)
