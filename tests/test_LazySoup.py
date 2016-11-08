6b2759e107cb88f1e6626b3e3d33eb4844b4aea7
jameswenzel <wenzel.james.r@gmail.com>
2016-11-07 23:58:57 -0800

    removed tor test, added LazySoup object test

--- tests/test_LazySoup.py
+++ tests/test_LazySoup.py
@@ -1,31 +1,25 @@
 import unittest
-from LazyScripts.LazyTor import TorConnection
 from LazyScripts import LazySoup
-try:
-    from torrc_password import PASSWORD
-except:
-    print('No torrc password found. Tests will fail.')
 
 
 class soupTest(unittest.TestCase):
 
-    def test_tor(self):
-        LazySoup.PASSWORD = PASSWORD
-        ip = 'http://icanhazip.com'
-        without_tor = LazySoup.get_soup(ip).text
-        with_tor = LazySoup.get_soup(ip, tor=True).text
-        self.assertNotEqual(without_tor, with_tor)
-        # close TorConnection
-        with TorConnection(password=PASSWORD):
-            pass
-
     @unittest.expectedFailure
     def test_fail(self):
+        '''Test failure on bad url'''
         LazySoup.get_soup('http://123fakestre.et')
 
     def test_berkeley(self):
-        goog = LazySoup.get_soup('http://berkeley.edu')
-        self.assertTrue(goog)
+        '''Test that get_soup parses a webpage correctly'''
+        berk = LazySoup.get_soup('http://berkeley.edu')
+        copyright = berk.select('div.copyright')
+        self.assertTrue(copyright)
+
+    def test_berkeley_obj(self):
+        '''Test that object's get_soup behaves the same way'''
+        berk = LazySoup.LazySoup().get_soup('http://berkeley.edu')
+        copyright = berk.select('div.copyright')
+        self.assertTrue(copyright)
 
 
 if __name__ == '__main__':
